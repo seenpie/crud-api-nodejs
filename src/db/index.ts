b6ddafb99068cb947c52ac203af54a521564938d
seenpie <bananananasez@gmail.com>
2024-10-21 20:18:05 +0300

    refactor: remove testUser

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,6 @@
 import { TUser } from "@/models/types";
 import { Storage } from "@/db/Classes/Storage";
-import { User } from "@/db/Classes/User";
 
-const testUser = new User("test user", 18, [
-  "riding on bike",
-  "watching films"
-]);
-
-const storeDB: TUser[] = [testUser.getUser()];
+const storeDB: TUser[] = [];
 
 export const storage = new Storage(storeDB);
