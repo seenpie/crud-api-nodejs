4d9e48b3945dfa1f3e22e2eac7e3f264373be9b1
seenpie <bananananasez@gmail.com>
2024-12-02 01:59:35 +0300

    refactor: change Storage instance

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import { Server } from "@/server/Classes/Server";
 import dotenv from "dotenv";
+import { TUser } from "./models/types";
+import { Storage } from "./db/Classes/Storage";
 
 dotenv.config();
 
-const server = new Server();
-
 function main() {
+  process.on("uncaughtException", console.log);
+
+  const store: TUser[] = [];
+  const dbService = new Storage(store);
+  const server = new Server(dbService);
+
   server.start(process.env.PORT);
 }
 
